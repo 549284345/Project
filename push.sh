cc25d36bc31dcfacf19a9195a9643b94dcd4797f
549284345 <liangyu.dai@foxmail.com>
2017-12-12 20:43:47 +0800

    home

---
+++ push.sh
@@ -0,0 +1,6 @@
+echo "======== push all folders ======== "
+git init
+git add .
+git commit -m "home"
+git push origin master
+read -p "Press any key to continue." var
--- test.sh
+++
@@ -1,7 +0,0 @@
-echo "======== update... all folders ======== "
-git init
-git add .
-curTime = date -d ¡°$currentTime¡± +%s 
-git commit -m curTime
-git push origin master  
-read -p "Press any key to continue." var
